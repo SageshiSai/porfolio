810bfcd66b822f4865040116bcb1977252cf05e6
SageshiSai <saikonogameryt@gmail.com>
2025-01-24 10:17:43 +0100

    links redes sociales

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa"
 
 export default function Footer() {
   return (
@@ -6,17 +6,17 @@ export default function Footer() {
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <p>&copy; 2023 Kevin leonardo patiño. Todos los derechos reservados.</p>
         <div className="flex space-x-4 mt-4 md:mt-0">
-          <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+          <a href="https://github.com/SageshiSai" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
             <FaGithub />
             <span className="sr-only">GitHub</span>
           </a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+          <a href="https://es.linkedin.com/in/kevin-leonardo-pati%C3%B1o-sanchez-a1b93032b" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
             <FaLinkedin />
             <span className="sr-only">LinkedIn</span>
           </a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
-            <FaTwitter />
-            <span className="sr-only">Twitter</span>
+          <a href="https://www.instagram.com/kevleopat/" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+            <FaInstagram />
+            <span className="sr-only">Instagram</span>
           </a>
         </div>
       </div>
