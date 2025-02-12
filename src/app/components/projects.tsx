5acff1997b7bdb7557b62b1c8737700e6a9e76db
SageshiSai <118272064+SageshiSai@users.noreply.github.com>
2025-02-12 21:09:29 +0100

    Cambio Imagen CommingSoon

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -20,14 +20,14 @@ const projects = [
   {
     title: "Tienda de ropa (En proceso)",
     description: "Mi primer pedido, parte de un familiar, el cual necesitaba una tienda online la cual pudiera subir todo el tipo de ropa que puede dar o arreglar ya que es consturera, un proyecto que me gsuto bastante.",
-    image: "/FFFFFF.png?height=auto&width=auto",
+    image: "/CommingSoon.png?height=auto&width=auto",
     demoLink: "#",
     codeLink: "#",
   },
   {
     title: "Inmobiliaria (En proceso)",
     description: "Gran proyecto que tengo pendiente para hacer gracias a un curso en le que estoy metido, un proyecto que me gusta bastante y que me gustaria hacerlo para poder tenerlo en mi portafolio y ganar a un más experiencia.",
-    image: "/FFFFFF.png?height=auto&width=auto",
+    image: "/CommingSoon.png?height=auto&width=auto",
     demoLink: "#",
     codeLink: "#",
   },
