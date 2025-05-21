ea5409440b98f53a99af98081fcc0e4e90c1ac21
SageshiSai <saikonogameryt@gmail.com>
2025-05-21 21:55:21 +0200

    Mejorar la visibilidad del componente Skills en dispositivos móviles y añadir un nuevo archivo para la gestión de visibilidad.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,26 @@ import Contact from "./components/contact"
 import Footer from "./components/footer"
 import {AuroraBackground} from "../components/ui/aurora-background"
 
+
 export default function Portfolio() {
+
+
+  
   return (
     <div className="min-h-screen bg-background text-foreground overflow-hidden">
       <Header />
       <main>
         <Hero />
         <About />
-        <AuroraBackground className="h-full">
+        
+        <AuroraBackground className="h-full hidden mb:block md:block">
           <div className="bg-black w-full h-full mb:h-5/6">
             <Skills />
           </div>
         </AuroraBackground>
+          <div className="bg-black w-full h-full mb:h-5/6 sm:block md:hidden">
+            <Skills />
+          </div>
         <Projects />
         <Contact />
       </main>
